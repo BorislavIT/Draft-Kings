b5334169504859bf2d585a73886c6dd5c8d00caa
Borislav Gegovski <borislav.gegovski@gmail.com>
2024-02-22 20:51:35 +0200

    made improvements to the pagination

--- src/components/Results/Pagination.tsx
+++ src/components/Results/Pagination.tsx
@@ -20,9 +20,10 @@ export const usePagination = () => {
 
   const onNewPageClicked = (
     newPage: number,
+    lastPage: number,
     setPage: (newPage: string) => void
   ) => {
-    if (newPage < 1) return;
+    if (newPage < 1 || newPage > lastPage) return;
     setPage(newPage.toString());
   };
 
@@ -41,21 +42,55 @@ const Pagination: FC<PaginationProps> = ({ totalPages }) => {
   const { page, setPage, onNewPageClicked } = usePagination();
 
   return (
-    <ul className="pagination flex flex-row flex-nowrap justify-end w-full text-white mt-4 h-12">
-      {Array.from({
-        length: totalPages,
-      }).map((_, index) => (
-        <li
-          key={index}
-          className={`border border-solid border-gray-200 hover:bg-gray-200 p-3 cursor-pointer bg-white text-black flex justify-center items-center ${
-            parseInt(page) === index + 1 && "text-[#1d4ed8] bg-[#eff6ff]"
-          }`}
-          onClick={() => onNewPageClicked(index + 1, setPage)}
-        >
-          {index + 1}
-        </li>
-      ))}
-    </ul>
+    <>
+      {totalPages && totalPages > 1 && (
+        <ul className="pagination flex flex-row flex-nowrap justify-end w-full text-white mt-4 h-12">
+          <li
+            className={`border border-solid border-gray-200 hover:bg-gray-200 p-3 cursor-pointer bg-white text-black flex justify-center items-center font-bold`}
+            onClick={() =>
+              onNewPageClicked(parseInt(page) - 1, totalPages, setPage)
+            }
+          >
+            &lt;
+          </li>
+          {Array.from({ length: totalPages }).map((_, index) => {
+            const pageIndex = index + 1;
+            // github copilot can take credit for this, it did a pretty good job
+            // i was showing all pages, but in the imaginary case that there are thousands of results
+            // we would have hundreds of pages so for the purpose of the assignment, it should be fine
+
+            // idea is to show first, second, current, second to last, last and up to 2 pages before and after the current page if they exist
+            const isRelevantPage =
+              pageIndex === 1 ||
+              pageIndex === 2 ||
+              pageIndex === totalPages - 1 ||
+              pageIndex === totalPages ||
+              (pageIndex >= parseInt(page) - 2 &&
+                pageIndex <= parseInt(page) + 2);
+
+            return isRelevantPage ? (
+              <li
+                key={index}
+                className={`border border-solid border-gray-200 hover:bg-gray-200 p-3 cursor-pointer bg-white text-black flex justify-center items-center font-bold ${
+                  parseInt(page) === pageIndex && "text-[#1d4ed8] bg-[#eff6ff]"
+                }`}
+                onClick={() => onNewPageClicked(pageIndex, totalPages, setPage)}
+              >
+                {pageIndex}
+              </li>
+            ) : null;
+          })}
+          <li
+            className={`border border-solid border-gray-200 hover:bg-gray-200 p-3 cursor-pointer bg-white text-black flex justify-center items-center font-bold`}
+            onClick={() =>
+              onNewPageClicked(parseInt(page) + 1, totalPages, setPage)
+            }
+          >
+            &gt;
+          </li>
+        </ul>
+      )}
+    </>
   );
 };
 
